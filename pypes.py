49358fd6ccdf9ba211d057ee45aeb85c8251132d
L3viathan <git@l3vi.de>
2016-08-17 15:12:26 +0200

    no idea

--- pypes.py
+++ pypes.py
@@ -3,6 +3,9 @@ from collections import deque
 class NoData(Exception):
     pass
 
+class Drain(Exception):
+    pass
+
 class Pipe(object):
     '''Generic pipe object. Add functions to put items in the pipe or something.'''
     def __init__(self, debug=False):
@@ -11,17 +14,27 @@ class Pipe(object):
         self.last = None
         self.debug = debug
     def __rshift__(self, something):
-        '''Let's for now assume it's a normal function. If not, we can also detect
-        that in __call__, but maybe we would want to do something here..'''
-        '''Yeah. Actually, we will want to transform that function here.'''
+        '''We assume that something is a processor.
+        If not, we can fail. Usage:
+        pipe >> something'''
+        something.drain = False
+        self.processors.append(something)
+        return self
+    def __matmul__(self, something):
+        '''Same as rshift, except: Set something to buffer.'''
+        something.drain = True
         self.processors.append(something)
         return self
     def __rrshift__(self, data):
         '''Not sure if we want this. This would mean:
-        [1,2,3] @ mypipe + foo("bar")
+        [1,2,3] >> mypipe >> foo("bar")
         or similar. Could only be done once, I guess?'''
         if self.debug: print("Adding input data: {}".format(repr(data)))
-        self.first = deque(data)
+        # If we give it a deque already, we use it and modify, otherwise copy
+        if isinstance(data, deque):
+            self.first = data
+        else:
+            self.first = deque(data)
         return self
     def __call__(self, debug = False):
         '''Process everything.
@@ -36,16 +49,24 @@ class Pipe(object):
         #while we still have processors:
         while self.processors:
             #loop through them,
-            for i, proc in enumerate(self.processors):
-                try:
-                    proc.process()
-                except NoData:
-                    if i == 0: #first generator?
-                        break
-                    else:
-                        continue # go to next processor
-            else: #nobreak
-                continue #start from the beginning again
+            try:
+                for i, proc in enumerate(self.processors):
+                    if proc.drain and i != 0:
+                        # start the while loop again. so break but nobreak?
+                        raise Drain
+                    try:
+                        proc.process()
+                    except NoData:
+                        if i == 0: #first generator?
+                            print("Was first generator")
+                            break
+                        else:
+                            print("Wasn't first generator")
+                            continue # go to next processor
+                else: #nobreak
+                    continue #start from the beginning again
+            except Drain:
+                continue
             #break
             if self.debug: print("Processor {} stopped".format(repr(self.processors[0])))
             self.processors.popleft()#remove leftmost processor
@@ -58,13 +79,28 @@ class Processor(object):
         '''Initialize settings from args.'''
         pass
     def set_in_out(self, in_):
+        '''You're not supposed to redefine this'''
         self.in_ = in_
-        #some mutable object
         self.out_ = deque()
         return self.out_
     def process(self):
-        '''Do with input, into output. If empty, raise NoData.'''
+        '''Do something with input, into output. If empty, raise NoData.
+        Subclasses of Processor should define this method, because otherwise,
+        they do nothing. Make sure to popleft from in_, and to append to out_'''
         pass
+    def pull(self):
+        '''Abstraction of popping left from input'''
+        try:
+            return self.in_.popleft()
+        except IndexError:
+            raise NoData
+    def push(self, data):
+        '''Abstraction of appending to output'''
+        self.out_.append(data)
+    def pushfrom(self, data):
+        '''Appending from iterable'''
+        for thing in data:
+            self.push(thing)
     def __rshift__(self, other):
         '''This means: No pipe class was there.
         We have to make a pipe and return it.'''
@@ -79,67 +115,65 @@ class Processor(object):
         return p >> self
 
 class AddTwo(Processor):
-    def __init__(self):
+    def __init__(self, debug=False):
         self.buf = None
+        self.debug = debug
     def process(self):
         while True:
-            try:
-                if self.buf==None:
-                    self.buf = self.in_.popleft()
-                self.out_.append(self.buf + self.in_.popleft())
-                #print("Appending {}".format(self.out_[-1]))
-                self.buf = None
-            except IndexError:
-                raise NoData
+            if self.buf==None:
+                self.buf = self.pull()
+            self.push(self.buf + self.pull())
+            if self.debug: print("Appending {}".format(self.out_[-1]))
+            self.buf = None
 
 class Map(Processor):
     '''Transform a true function into a processor'''
-    def __init__(self, fn):
+    def __init__(self, fn, debug=False):
         self.fn = fn
+        self.debug = debug
     def process(self):
-        try:
-            while True:
-                e = self.in_.popleft()
-                #print("Appending {}".format(self.fn(e)))
-                self.out_.append(self.fn(e))
-        except IndexError:
-            raise NoData
+        while True:
+            e = self.pull()
+            if self.debug: print("Appending {}".format(e))
+            self.push(self.fn(e))
     def __repr__(self):
         return '<Map: {}>'.format(repr(self.fn))
 
 class Filter(Processor):
     '''Transform a true function into a processor'''
-    def __init__(self, fn):
+    def __init__(self, fn, debug=False):
         self.fn = fn
+        self.debug = debug
     def process(self):
-        try:
-            while True:
-                e = self.in_.popleft()
-                if self.fn(e):
-                    #print("Appending {}".format(e))
-                    self.out_.append(e)
-        except IndexError:
-            raise NoData
+        while True:
+            e = self.pull()
+            if self.fn(e):
+                if self.debug: print("Appending {}".format(e))
+                self.push(e)
     def __repr__(self):
         return '<Filter: {}>'.format(repr(self.fn))
 
 class Reduce(Processor):
     '''Transform a true function into a processor'''
-    def __init__(self, fn):
+    def __init__(self, fn, start=None, debug=False):
         self.fn = fn
         self.cur = None
+        self.start = start
+        self.debug = debug
     def process(self):
+        print(self.cur)
+        if self.start == None:
+            self.cur = self.pull()
+        else:
+            self.cur = self.start
         try:
-            self.cur = self.in_.popleft()
-            try:
-                while True:
-                    e = self.in_.popleft()
-                    self.cur = self.fn(self.cur, e)
-            except IndexError:
-                #print("Appending {}".format(self.cur))
-                self.out_.append(self.cur)
-                raise NoData
-        except IndexError:
+            while True:
+                e = self.pull()
+                if self.debug: print("Pulled {}".format(e))
+                self.cur = self.fn(self.cur, e)
+        except NoData:
+            if self.debug: print("Appending {}".format(self.cur))
+            self.push(self.cur)
             raise NoData
     def __repr__(self):
         return '<Reduce: {}>'.format(repr(self.fn))
@@ -151,15 +185,45 @@ class UserInput(Processor):
         i = input("Enter some numbers seperated by spaces: ")
         if len(i) < 2:
             raise NoData
-        self.out_.extend(map(int,i.split(" ")))
+        self.pushfrom(map(int, i.split(" ")))
+
+class Bufferer(Processor):
+    def __init__(self, number):
+        self.number = number
+    def process(self):
+        buffr = []
+        try:
+            for i in range(self.number):
+                buffr.append(self.pull())
+                #print(buffr)
+            for e in buffr:
+                self.push(e)
+        except NoData:
+            for e in buffr:
+                self.push(e)
+            raise NoData
+
+class Printer(Processor):
+    def process(self):
+        print("Printing:")
+        while True:
+            e = self.pull()
+            print(e)
+            self.push(e)
 
 if __name__ == '__main__':
+    '''
     mypipe = UserInput() >> Filter(lambda x: x%2==0) >> Map(str) >> Reduce(lambda x, y: x+y) >> Map(int)
     for res in mypipe():
         print(res)
+    '''
 
-    mypipe = range(100) >> AddTwo() >> Filter(lambda x: (x+1)%10==0) >> AddTwo() >> Map(str) >> Reduce(lambda x, y: x+y) >> Map(int)
-    for res in mypipe():
+    #mypipe = range(100) >> AddTwo() >> Filter(lambda x: (x+1)%10==0) >> AddTwo() >> Map(str) >> Reduce(lambda x, y: x+y) >> Map(int)
+    mypipe = (range(100) >> Bufferer(10)) >> Printer() >> Reduce(lambda x,y: x+y)
+    print(mypipe)
+    print(mypipe.processors)
+    input()
+    for res in mypipe(debug=True):
         print(res)
 
     '''
@@ -169,3 +233,8 @@ if __name__ == '__main__':
 
     words = dict(mypipe())
     '''
+    #if we want some part to block, we could do:
+    #mypipe = (Something() >> Somethong() >> Foo())() >> Bar() >> Bat()
+    # but that would execute the first part here already... hmm
+    # other option would be to declare processors blocking... could also use a different operator then?
+    #mypipe = Something() >> Somethong() >> Foo() @ Bar() >> Bat()
